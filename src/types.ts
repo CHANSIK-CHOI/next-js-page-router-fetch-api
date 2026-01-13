4b96dbe0fef76987b3f74dbca6ce32aa3dfb6bf5
CHANSIK-CHOI <ckstlr0828@frameout.co.kr>
2026-01-13 22:42:11 +0900

    [최찬식] 빌드 에러 해결

--- src/types.ts
+++ src/types.ts
@@ -1,34 +1,34 @@
 export type User = {
-  avatar: string
-  email: string
-  first_name: string
-  id: number
-  last_name: string
-}
+  avatar: string;
+  email: string;
+  first_name: string;
+  id: number;
+  last_name: string;
+};
 
-type UserKeys = keyof User
+type UserKeys = keyof User;
 // key
-export type EditableUserKey = Exclude<UserKeys, 'id'>
-type RequiredEditableUserKey = Exclude<EditableUserKey, 'avatar'>
+export type EditableUserKey = Exclude<UserKeys, "id">;
+type RequiredEditableUserKey = Exclude<EditableUserKey, "avatar">;
 
 // object
-export type InitUserFormObject = Pick<User, RequiredEditableUserKey> & { avatar?: User['avatar'] }
-type EditableUserFormObject = Partial<Omit<User, 'id'>>
+export type InitUserFormObject = Pick<User, RequiredEditableUserKey> & { avatar?: User["avatar"] };
+type EditableUserFormObject = Partial<Omit<User, "id">>;
 
 // POST
-export type PayloadNewUser = InitUserFormObject
-export type ApiResultNewUser = User & { createdAt: string }
+export type PayloadNewUser = InitUserFormObject;
+export type ApiResultNewUser = User & { createdAt: string };
 
 // PATCH
-export type PayloadModifiedUser = EditableUserFormObject
-export type ApiResultModifiedUser = PayloadModifiedUser & { updatedAt: string }
-export type PayloadAllModifiedUsers = { id: User['id']; payload: EditableUserFormObject }[]
+export type PayloadModifiedUser = EditableUserFormObject;
+export type ApiResultModifiedUser = PayloadModifiedUser & { updatedAt: string };
+export type PayloadAllModifiedUsers = { id: User["id"]; payload: EditableUserFormObject }[];
 export type ApiResultAllModifiedUsers = {
-  id: User['id']
-  result: ApiResultModifiedUser
-}[]
+  id: User["id"];
+  result: ApiResultModifiedUser;
+}[];
 
 // react form hooks
 export type UsersFormValues = {
-  users: User[]
-}
+  users: User[];
+};
